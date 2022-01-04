696712c9e6fb4ede770c6602683d242119199caa
James Hagood <cardfaux@tutanota.com>
2022-01-04 08:40:27 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node and React 🎉</Heading>
+    <h1>testing compile</h1>
+  </Page>
+);
+
+export default Index;
